5ebbfef047beb94cc35c28b023aefda9ae6b70d2
anwar zigler <77289226+Zigler1@users.noreply.github.com>
2021-01-11 18:26:29 +0100

    Add files via upload

---
+++ AdsAdmob/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using MarcTron.Plugin;
+
+namespace AdsAdmob
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            CrossMTAdmob.Current.OnInterstitialLoaded += (sender, e) => { CrossMTAdmob.Current.ShowInterstitial(); };
+            CrossMTAdmob.Current.OnRewardedVideoAdLoaded += (sender, e) => { CrossMTAdmob.Current.ShowRewardedVideo(); };
+        }
+
+        private void interstitial_Clicked(object sender, EventArgs e)
+        {
+            CrossMTAdmob.Current.LoadInterstitial("Interstitial Ad Id");
+            
+        }
+
+        private void reward_Clicked(object sender, EventArgs e)
+        {
+            CrossMTAdmob.Current.LoadRewardedVideo("Reward Video Ad Id");
+        }
+    }
+}
+
